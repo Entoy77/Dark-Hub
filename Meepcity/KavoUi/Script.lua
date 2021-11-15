f3e5c07936e88726f345597f74edf2ab9addc57f
Souls <94298035+Entoy77@users.noreply.github.com>
2021-11-15 20:40:39 -0300

    Create Script.lua

---
+++ Meepcity/KavoUi/Script.lua
@@ -0,0 +1,17 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("DarkHub", "DarkTheme")
+
+local Tab = Window:NewTab("MeepCity")
+local Section = Tab:NewSection(" ")
+
+Section:NewButton("Free Plus", "a", function()
+ game.ReplicatedStorage.PlayerData[game.Players.LocalPlayer.UserId].PLUS.Value = true
+end)
+
+Section:NewButton("Free BoomBox GamePass", "a", function()
+ game.ReplicatedStorage.PlayerData[game.Players.LocalPlayer.UserId].BoomBox.Value = true
+end)
+
+Section:NewButton("Give Free Money", "a", function()
+   loadstring(game:HttpGet("https://pastebin.com/raw/i9XDEKVW"))()
+end)
