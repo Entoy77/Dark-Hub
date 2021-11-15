85c5fad4ec93d629aee94d49e4c310a1506c42bc
Souls <94298035+Entoy77@users.noreply.github.com>
2021-11-15 20:42:51 -0300

    Create DarkHubHome.lua

---
+++ DarkHub_Assets/.../DarkHubHome.lua
@@ -0,0 +1,27 @@
+local Material = loadstring(game:HttpGet("https://raw.githubusercontent.com/Kinlei/MaterialLua/master/Module.lua"))()
+
+local UI = Material.Load({
+     Title = "Dark Hub - Loader",
+     Style = 3,
+     SizeX = 400,
+     SizeY = 100,
+     Theme = "Dark"
+})
+
+local Page = UI.New({
+    Title = "Loader"
+})
+
+Page.Button({
+    Text = "Load",
+    Callback = function()
+       loadstring(game:HttpGet("https://pastebin.com/raw/satH3205"))()
+    end
+})
+
+Page.Button({
+    Text = "Open Credits",
+    Callback = function()
+       loadstring(game:HttpGet("https://raw.githubusercontent.com/Entoy77/Dark-Hub/main/Darkl.api/LOL/Pages/CreditPage"))()
+    end
+})
