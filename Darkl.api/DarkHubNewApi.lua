16630865d4df988184c574590677fad32cd2eecc
Souls <94298035+Entoy77@users.noreply.github.com>
2021-11-21 10:37:44 -0300

    Create DarkHubNewApi.lua

---
+++ Darkl.api/DarkHubNewApi.lua
@@ -0,0 +1,14 @@
+local NotificationBindable = Instance.new("BindableFunction")
+local Msgreq = function(Title,Text,Duration,Button1Text,Button2Text)
+    game.StarterGui:SetCore("SendNotification", {
+        Title = Title;
+        Text = Text;
+        Icon = "http://www.roblox.com/asset?id=8061259309";
+        Duration = Duration;
+        Button1 = Button1Text;
+        Button2 = nil;
+        Callback = NotificationBindable;
+    })
+end
+ 
+Msgreq("Darkl.Api","Made by Visual Studio",5,nil)
